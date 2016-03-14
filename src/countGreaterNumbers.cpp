9fb590b433e64f697eaf9f719f1892a5287380b7
vijaysamanthapudi <vijaysamanthapudi@missionrnd.com>
2016-03-14 22:29:55 +0530

    Submit Commit

--- src/countGreaterNumbers.cpp
+++ src/countGreaterNumbers.cpp
@@ -13,7 +13,9 @@ ERROR CASES: Return NULL for invalid inputs.
 
 NOTES:
 */
-
+void convert_date(char str[], int d[]);
+int search(struct transaction *Arr, int left, int right, int d1[], int dlen);
+int compare(int d1[], int d2[], int dlen);
 struct transaction {
 	int amount;
 	char date[11];
@@ -21,5 +23,102 @@ struct transaction {
 };
 
 int countGreaterNumbers(struct transaction *Arr, int len, char *date) {
-	return -1;
+	
+	int d1[3] = { 0 }, d2[3] = { 0 }, i;
+	convert_date(date, d1);
+	convert_date(Arr[len - 1].date, d2);
+	if (compare(d1, d2, 3) == 1)
+	{
+		return 0;
+	}
+	int pos = search(Arr, 0, len, d1, 2);
+	if (pos == -1)
+	{
+		return 0;
+	}
+	else
+	{
+		return len - pos - 1;
+	}
+
+
+
+}
+void convert_date(char str[], int d[])
+{
+	int i, j = 0;
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] == '-')
+		{
+			j++;
+		}
+		else
+		{
+			d[j] = d[j] * 10 + str[i] - '0';
+		}
+
+
+	}
+
+}
+
+
+int search(struct transaction *Arr, int left, int right, int d1[], int dlen)
+{
+	if (left <= right)
+	{
+
+		int d2[3] = { 0 }, pos = left + right / 2;
+		
+		convert_date(Arr[pos].date, d2);
+		int choice = compare(d1, d2, dlen);
+		switch (choice)
+		{
+		case 1:return pos;
+			break;
+		case -1:
+			search(Arr, left, pos - 1, d1, dlen);
+			break;
+		case -2:
+			search(Arr, pos + 1, right, d1, dlen);
+			break;
+		case 3:
+			return -1;
+		}
+	}
+	else
+	{
+		return -1;
+	}
+
+
+}
+
+
+int compare(int d1[], int d2[], int dlen)
+{
+
+	if (dlen == 0 && d1[dlen] == d2[dlen])
+	{
+		return 1;
+	}
+	if (dlen == 0 && d1[dlen] != d2[dlen])
+	{
+		return 3;
+	}
+	else if (d1[dlen] < d2[dlen])
+	{
+		return -1;
+	}
+	else if (d1[dlen] > d2[dlen])
+	{
+		return -2;
+	}
+	else if (d1[dlen] == d2[dlen])
+	{
+		compare(d1, d2, --dlen);
+	}
+
+
 }
